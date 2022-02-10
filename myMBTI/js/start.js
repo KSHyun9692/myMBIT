346b57cbcba3fc9c81afa1da4ad67b19a08ad7fe
KSHyun9692 <KSHyun9692@gmail.com>
2022-02-10 16:14:09 +0900

    MBTI

--- myMBTI/js/start.js
+++ myMBTI/js/start.js
@@ -3,13 +3,13 @@ const qna = document.querySelector("#qna");
 
 function begin(){
     main.style.WebkitAnimation = "fadeOut 1s";
-    main.style.animation = "fateOut 1s";
+    main.style.animation = "fadeOut 1s";
     setTimeout(() => {
         qna.style.WebkitAnimation = "fadeIn 1s";
         qna.style.animation = "fadeIn 1s";
-    
         setTimeout(() => {
             main.style.display = "none";
+             qna.style.display = "block"
         }, 450)
-    }, 450)
+    }, 450);
 }
